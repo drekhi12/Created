268d8c9daa2fba38a1ecb56a9b09d05f33be5abb
drekhi12 <drekhi12@gmail.com>
2016-10-31 17:16:37 +0530

    update

--- python/pdf_manga.py
+++ python/pdf_manga.py
@@ -4,6 +4,37 @@ import urllib2
 from reportlab.platypus import (SimpleDocTemplate, Paragraph, PageBreak)
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib.pagesizes import LETTER
+from reportlab.pdfgen import canvas
+
+class FooterCanvas(canvas.Canvas):
+
+    def __init__(self, *args, **kwargs):
+        canvas.Canvas.__init__(self, *args, **kwargs)
+        self.pages = []
+
+    def showPage(self):
+        self.pages.append(dict(self.__dict__))
+        self._startPage()
+
+    def save(self):
+        page_count = len(self.pages)
+        for page in self.pages:
+            self.__dict__.update(page)
+            self.draw_canvas(page_count)
+            canvas.Canvas.showPage(self)
+        canvas.Canvas.save(self)
+
+    def draw_canvas(self, page_count):
+        page = "Page %s of %s" % (self._pageNumber, page_count)
+        x = 128
+        self.saveState()
+        self.setStrokeColorRGB(0, 0, 0)
+        self.setLineWidth(0.5)
+        self.line(66, 78, LETTER[0] - 66, 78)
+        self.setFont('Times-Roman', 10)
+        self.drawString(LETTER[0]-x, 65, page)
+        self.restoreState()
+
 
 
 url="http://japtem.com/arifureta-volume-4-chapter-8/"
@@ -21,11 +52,12 @@ for p in para:
 
 # Content
 styles = getSampleStyleSheet()
+print styles
 elements = []
-elements.append(Paragraph(header, styles["Normal"]))
+elements.append(Paragraph(header, styles["Title"]))
 elements.append(Paragraph("", styles["Normal"]))
 for i in content:
-    elements.append(Paragraph(i, styles["Normal"]))
+    elements.append(Paragraph(i, styles["BodyText"]))
 # Build
 doc = SimpleDocTemplate("manga.pdf", pagesize=LETTER)
-doc.build(elements)
+doc.multiBuild(elements, canvasmaker=FooterCanvas)
